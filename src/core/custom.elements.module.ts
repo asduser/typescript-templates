27c969737ccfab4e8089d873943e5ea48a7b5ef3
Djingo <mailerboss00@mail.com>
2017-07-27 23:33:56 +0300

    [+] jQuery

---
+++ src/core/custom.elements.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: []
+})
+
+export class CustomElementsModule { }
