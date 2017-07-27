85a12bc65de2704516b21c6d79e13c27161e9ade
Djingo <mailerboss00@mail.com>
2017-07-28 00:13:09 +0300

    [+] Directives: hasRole.

--- src/core/custom.elements.module.ts
+++ src/core/custom.elements.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {HasRoleDirective} from "./directives/has-role/has-role.directive";
 
 @NgModule({
   declarations: [
-
+    HasRoleDirective
+  ],
+  exports: [
+    HasRoleDirective
   ],
   imports: [
     BrowserModule,
--- src/core/directives/has-role/has-role.directive.ts
+++ src/core/directives/has-role/has-role.directive.ts
@@ -1,3 +1,49 @@
-/**
- * Created by Zibert on 27.07.2017.
- */
+import {Directive, Input, ElementRef, OnChanges, SimpleChanges, ViewContainerRef, TemplateRef, OnInit} from "@angular/core";
+
+@Directive({
+  selector: '[hasRole]'
+})
+export class HasRoleDirective implements OnInit, OnChanges {
+
+  @Input() hasRole:Array<string> = [];
+  private role:string;
+
+  constructor(private elementRef:ElementRef,
+              private viewContainerRef:ViewContainerRef,
+              private template:TemplateRef<any>
+  ) {}
+
+  ngOnInit() {}
+
+  ngOnChanges(changes:SimpleChanges):void {
+    let hasRoleChanges = changes['hasRole'];
+    if (hasRoleChanges) {
+      this.hasRole = hasRoleChanges.currentValue;
+      this.update();
+    }
+  }
+
+  private update():void {
+    // in the real app, should be set by the real application user's role
+    this.role = 'user';
+
+    this.checkRoles(this.role);
+  }
+
+  private checkRoles(userRole:string) {
+    if (!this.hasRole.length || !!~this.hasRole.indexOf(userRole)) {
+      this.createView();
+    } else {
+      this.clearView();
+    }
+  }
+
+  private createView():void {
+    this.viewContainerRef.createEmbeddedView(this.template);
+  }
+
+  private clearView():void {
+    this.viewContainerRef.clear();
+  }
+
+}
